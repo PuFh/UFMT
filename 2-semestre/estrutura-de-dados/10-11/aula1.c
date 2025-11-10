73fbac93250ad91db7c7ee1cfd2a31f7f1f01b5a
Matheus <137668343+PuFh@users.noreply.github.com>
2025-11-10 09:22:36 -0300

    aula1.c
    Lista encadeada

---
+++ 2-semestre/estrutura-de-dados/10-11/aula1.c
@@ -0,0 +1,9 @@
+/* 
+fila/pilha/lista (com alocação dinamica)
+
+--------LISTA ENCADEADA---------
+
+-melhor uso da memoria
+-a estrutura se amplia a medida que os dados sao inseridos
+-usa o principio de encadeamento
+*/
