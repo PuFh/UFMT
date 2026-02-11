fd50c6723ccaa3cfbf68cc99485a9ad9328f842c
Matheus <137668343+PuFh@users.noreply.github.com>
2026-02-11 10:01:53 -0300

    making a binary tree

---
+++ 2-semestre/estrutura-de-dados/2026/11-09/arvore_binaria.c
@@ -0,0 +1,49 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+typedef struct arvore_binaria{
+    int valor;
+    struct arvore_binaria* fEsq;
+    struct arvore_binaria* fDir;
+}arvore_binaria;
+
+//inicializacao
+//procedimento recursivo de colocar o valor na posicao
+
+//PROTOTIPO
+int insercao(arvore_binaria*, int);
+
+int main(){
+    arvore_binaria tree;
+
+    insercao(&tree,15);
+    insercao(&tree,20);
+    insercao(&tree,8);
+
+    printf("correto");
+    printf("%d",tree.valor);
+    printf("%d",tree.fEsq->valor);
+
+    return true;
+}
+
+int insercao(arvore_binaria *temp, int v){
+    //caso unico, em que estamos na raiz
+    
+    if(temp == NULL){
+        //aloca memoria e guarda
+        temp = (arvore_binaria*)malloc(sizeof(arvore_binaria*));
+        temp->valor = v;
+        temp->fDir = NULL;
+        temp->fEsq = NULL;
+    }else{
+        if(v > temp->valor){//filho direto
+            insercao(temp->fDir, v);
+        }
+        else{
+            insercao(temp->fEsq, v);
+        }
+    }
+
+    
+}
