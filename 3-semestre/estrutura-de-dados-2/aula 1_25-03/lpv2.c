1875be10e1435df9df7bb56d4d71bd2f7db666fe
Matheus <137668343+PuFh@users.noreply.github.com>
2026-03-25 21:52:17 -0300

    lpv2

---
+++ 3-semestre/estrutura-de-dados-2/aula 1_25-03/lpv2.c
@@ -0,0 +1,91 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef struct lista{
+    int valor;
+    struct lista* prox;
+}lista;
+
+//Implemente a função que insere valores no fim da lista encadeada
+
+//prototipo
+lista* criaNo(lista*, int);
+lista* insereFim(lista*,int);
+lista* insereInicio(lista*, int);
+void imprime(lista*);
+
+int main(){
+    lista* head;
+
+    head = criaNo(head, 10);
+    head = insereInicio(head, 13);
+    imprime(head);
+
+    head = insereFim(head, 12);
+    imprime(head);
+}
+
+lista* criaNo(lista* li, int v){
+    lista* novo = (lista*)malloc(sizeof(lista));
+
+    if(novo==NULL){
+        printf("Error ao alocar memoria dinamicamente!");
+    }
+    novo->valor = v;
+    novo->prox = NULL;
+    return novo;
+
+}
+
+//impressoa
+void imprime(lista* li){
+
+    if(li == NULL){
+        printf(" NuLL\nFim da Lista!");
+        return;
+    }
+    printf("%d --> ",li->valor);
+    imprime(li->prox);
+}
+
+lista* insereInicio(lista* li, int v){
+    //caso a lista esteja vazia
+    lista* p;
+    if(li==NULL){
+       p =  criaNo(li, v);
+       return p;
+    }
+    lista* novo = (lista*)malloc(sizeof(lista));
+    novo->valor = v;
+    novo->prox = li;
+    return novo;
+
+
+}
+
+//insere no fim(O QUE VIEMOS FAZER)
+lista* insereFim(lista* li, int v){
+    //caso a nossa lista esteja vazia
+    if(li == NULL){
+       lista* p =  criaNo(li, v);
+       return p;
+    }
+    
+
+    lista* novo = (lista*)malloc(sizeof(lista));
+
+    lista* p = li;
+    lista* q = li;
+
+    while( q != NULL){
+        p = q;
+        q = q->prox;  
+    }
+    printf("aqui\n");
+    if(q == NULL){
+        p->prox = novo;
+    }else{
+        li = novo;
+        return li;
+    }
+}
