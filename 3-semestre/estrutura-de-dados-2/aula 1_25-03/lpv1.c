56f0edbc2363ee45852b716e63aea5a38290af0e
Matheus <137668343+PuFh@users.noreply.github.com>
2026-03-26 13:43:23 -0300

    making ex1

---
+++ 3-semestre/estrutura-de-dados-2/aula 1_25-03/lpv1.c
@@ -0,0 +1,119 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+//struct
+typedef struct lista{
+    int valor;
+    struct lista* prox;
+}lista;
+
+
+//prototipo
+lista* criaNo(int);
+//lista* insereFim(lista*,int);
+lista* insereInicio(lista*, int);
+void imprime(lista*);
+lista* insereOrdem(lista*,int);
+
+//main
+int main(){
+    lista* head;
+
+    head = criaNo(23);
+
+    head = insereOrdem(head, 20);
+
+    head = insereOrdem(head, 13);
+    head = insereOrdem(head, 7);
+    imprime(head);
+
+
+}
+
+lista* criaNo(int v){
+    lista* novo = (lista*)malloc(sizeof(lista));
+
+    if(novo == NULL){
+        printf("erro ao alocar memoria!");
+        return 0;
+    }
+    novo->valor = v;
+    novo->prox = NULL;
+    return novo;
+}
+
+lista* insereInicio(lista* li, int v){
+    
+    if(li == NULL){
+        lista* novo = criaNo(v);
+        return novo;
+    }
+
+    lista* novo = (lista*)malloc(sizeof(lista));
+    novo->valor = v;
+    novo->prox = li;
+    return novo;
+}
+
+
+void imprime(lista* li){
+    if(li == NULL){
+        printf("--> NULL\nFim da Lista!");
+        return;
+    }
+    printf("%d ",li->valor);
+    imprime(li->prox);
+}
+
+lista* insereOrdem(lista* li, int v){
+
+    if(li == NULL || li->valor < v){
+        lista* novo = criaNo(v);
+        novo->prox = li;
+        return novo;
+    }
+
+
+    lista* p = li;
+    lista* q = li;
+
+    lista* novo = (lista*)malloc(sizeof(lista));
+    novo->valor = v;
+    novo->prox = li;
+
+   
+
+    while(p->prox != NULL && p->valor < novo->valor ){
+
+        //avanca um bloco
+        q = p;
+        p = p->prox;
+    }
+
+    novo->prox = q->prox;
+    q->prox = novo;
+    return li;
+
+}
+/*
+lista* insereOrdem(lista* li, int v) {
+    lista* novo = criaNo(v);
+
+    // lista vazia ou inserir antes do head
+    if (li == NULL || v < li->valor) {
+        novo->prox = li;
+        return novo;
+    }
+
+    // percorre até achar a posição certa
+    lista* p = li;
+    while (p->prox != NULL && p->prox->valor < v) {
+        p = p->prox;
+    }
+
+    novo->prox = p->prox;
+    p->prox = novo;
+
+    return li; // head não mudou
+}
+*/
