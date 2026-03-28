1f656107c0d3f81cc234c3244d33f772b9aa6165
Matheus <137668343+PuFh@users.noreply.github.com>
2026-03-27 23:25:15 -0300

    add function alterar

--- repertorio/ed1/lista_ordenada.c
+++ repertorio/ed1/lista_ordenada.c
@@ -10,16 +10,80 @@ typedef struct no{
 no* criaNo(int);
 void imprimeLista(no*);
 no* insereOrdem(no*, int);
+no* exclui(no*, int);
+no* alterar(no*, int, int);
 
 //funcao principal
 int main(){
 
-    no* lista = criaNo(8);
+    no* lista = criaNo(0);
     
-    lista = insereOrdem(lista, 9);
-    lista = insereOrdem(lista, 2);
+    
+    
+    int opcao = 0;
+    
+    while (opcao != 4){
+        
+        printf("-------------------\n");
+        printf("| LISTA ORDENADA  |\n");
+        printf("-------------------\n");
+        printf("1 -> Adicionar\n");
+        printf("2 -> imprimir\n");
+        printf("3 -> Excluir\n");
+        printf("4 -> Sair\n");
+        printf("\n-->");
+        scanf("%d",&opcao);
+        
+        switch (opcao)
+        {
+        case 1:
+            printf("Digite o valor no qual voce deseja adicionar: ");
+            int valor;
+            scanf("%d",&valor);
+    
+            lista = insereOrdem(lista, valor);
+            
+            break;
+        case 2:
+    
+            imprimeLista(lista);
+            break;
 
-    imprimeLista(lista);
+        case 3:
+            printf("Digite o valor no qual voce deseja remover: ");
+            int remove;
+            scanf("%d",&remove);
+    
+            lista = exclui(lista, remove);
+            break;
+        
+        case 4:
+            printf("Digite o valor no qual voce deseja substituir:");
+            int antigo;
+            scanf("%d",&antigo);
+
+            printf("Qual voce ira ficar no lugar?");
+            int novo;
+            scanf("%d", &novo);
+            
+
+            lista = alterar(lista, antigo, novo);
+            
+
+            break;
+
+        case 5:
+    
+            return 1;
+            break;
+        
+        
+        default:
+            printf("Opcao Invalida\n");
+            break;
+        }
+       
+    }
 
 }
 
@@ -29,7 +93,7 @@ no* criaNo(int v){
     no* novo = (no*)malloc(sizeof(no));
 
     if(novo == NULL){
-        printf("Error");
+        printf("Error\n");
         return 0;
     }
 
@@ -41,7 +105,7 @@ no* criaNo(int v){
 //funcao imprime
 void imprimeLista(no* li){
     if(li == NULL){
-        printf(" NULL\nFim da Lista");
+        printf(" NULL\nFim da Lista\n");
         return;
     }
 
@@ -78,3 +142,76 @@ no* insereOrdem(no* li, int v){
     return li;
 }
 
+//funcao remove valor
+no* exclui(no* li, int excluir){
+
+    //caso o valor seja o primeiro
+    if(li->valor == excluir){
+    no* temp = li;
+    li = li->prox;
+    free(temp);
+    return li;
+    }
+
+
+    no* prox = li;
+    no* ant = li;
+    //faremos um laco para percorrer a lista se for diferente de nula ou se o valor que queremos excluir for diferente do valor da posicao que a lista esta
+    while(prox != NULL && prox->valor != excluir){
+        if(prox->valor == excluir){
+            ant->prox = prox->prox;//isso faz com que pulemos o bloco que queremos excluir(que é somento o prox)
+            free(prox);
+
+            return li;
+        }
+        else{
+            ant= prox;
+            prox = prox->prox;
+        }
+
+    }
+    printf("Valor não existe na sua lista!\n");
+    return li;
+}
+
+
+//funcao alterar
+no* alterar(no* li, int vAntigo, int vNovo){
+
+    //caso 1: valor inicial da lista for oq iremos substituir
+    if(li->valor == vAntigo){
+        li->valor = vNovo;
+        //verificar se esta ordenado
+        li = exclui(li, vAntigo);
+        li = insereOrdem(li, vNovo);
+
+        return li;
+    }
+
+    //caso 2: percorrer a lista ate encontrar o valor para sub
+    no* prox = li;
+    no* ant = li;
+
+    while (prox != NULL && prox->valor != vAntigo){
+        //avancamos uma posicao e o ant fica na posicao antiga para n perdermos a referencia
+        ant = prox;
+        prox = prox->prox;
+    }
+    
+    //se chegamos ate aqui temos 2 casos, 1 eh se a prox = null, entao n encontramos o valor, o 2 eh se encontramos
+    if(prox == NULL){
+        printf("Valor nao existe\n");
+        return li;
+    }else{
+        //o valor existe, entao prox->valor é = a vantigo
+        prox->valor = vNovo;
+
+        //verificar se esta ordenado
+        li = exclui(li, vAntigo);
+        li = insereOrdem(li, vNovo);
+
+        return li;
+    }
+
+    
+}
