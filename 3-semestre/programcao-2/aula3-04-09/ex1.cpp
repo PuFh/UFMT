7e940bf95263ba42fecce24d3fa93c89036c52b2
Matheus <137668343+PuFh@users.noreply.github.com>
2026-04-10 00:45:59 -0300

    making exercise from prog 2

---
+++ 3-semestre/programcao-2/aula3-04-09/ex1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    int idade, nivel;
+    bool autorizacao;
+    std::string at;
+
+    std::cout << "Digite  a sua idade: "<<std::endl;
+    std::cin>>idade;
+    std::cin.ignore(1000, '\n'); 
+    
+
+    if(idade < 16){
+        std::cout <<"Acesso negado!"<<std::endl;
+        return 0;
+    }
+
+    std::cout <<"Voce tem autorizacao?"<< std::endl;
+    std::getline(std::cin, at);
+
+    if((at == "sim" || at == "s")){
+        autorizacao = 1;
+    }
+    else{
+        autorizacao = 0;
+    }
+    //condicao para
+
+    if((16 <= idade <=17 && autorizacao) || idade > 17){
+        std::cout <<"Digite o nivel" <<std::endl;
+        std::cin >>nivel;
+        std::cin.ignore(1000, '\n'); 
+
+        switch (nivel){
+        case 1:
+            std::cout <<"NIvel basico";
+            break;
+        case 2:
+            std::cout <<"NIvel intermediario";
+            break;
+        case 3:
+            std::cout <<"NIvel avancado";
+            break;
+        default:
+            std::cout <<"Opcao invalida";
+            break;
+        }
+    }else{
+        std::cout <<"N tem autorizacao"<<std::endl;
+    }
+    return 0;
+
+}
---
+++ 3-semestre/programcao-2/aula3-04-09/ex2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    int saldo, menu,ax;
+    saldo = 1000;//saldo inicial
+    bool condi = 1;
+
+    while(condi){
+        std::cout <<"-------------"<<std::endl;
+        std::cout <<"1. ver saldo"<<std::endl;
+        std::cout <<"2. Depositar"<<std::endl;
+        std::cout <<"3. Sacar"<<std::endl;
+        std::cout <<"4. Sair"<<std::endl;
+        std::cout <<"-------------"<<std::endl;
+        std::cin >>menu;
+        switch(menu){
+        case 1:
+            //ver saldo
+            std::cout << "Saldo Atual: "<< saldo<<std::endl;
+
+            break;
+        case 2:
+            //depositar
+            std::cout <<"Valor a ser depositado: ";
+            std::cin >>ax;
+            saldo += ax;
+            break;
+        case 3:
+            //sacar
+            std::cout <<"valor a ser sacado: ";
+            std::cin >> ax;
+            //condicao caso o valor seja maior q tem na conta
+            if(ax > saldo){
+                std::cout <<"Valor a ser sacado insuficiente" << std::endl;
+                
+            }else{
+                saldo -= ax;
+            }
+            break;
+        case 4:
+            //sair
+            return 1;
+            break;
+        default:
+            std::cout <<"Opcao invalida, selecione uma das opcoes"<< std::endl;
+            break;
+        }
+
+    }
+
+
+}
