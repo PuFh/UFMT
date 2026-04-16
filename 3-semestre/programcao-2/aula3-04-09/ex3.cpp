d2bf20372b899cfb589c1593edd9809d4f3f7b51
Matheus <137668343+PuFh@users.noreply.github.com>
2026-04-16 14:04:27 -0300

    making ex3

--- 3-semestre/programcao-2/aula3-04-09/ex3.cpp
+++ 3-semestre/programcao-2/aula3-04-09/ex3.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 
+int main(){
+    int n,soma;
+
+    std::cin >> n;
+
+    if( n > 100 ){
+        std::cout <<"Valor muito grande!\n";
+        return 0;
+    }
+    for(int i = 1; i < n;i++){
+        if(n%i == 0){
+            soma +=i;
+        }
+    }
+    if(soma == n){
+        std::cout <<"Numero perfeito\n";
+    }else{
+        std::cout <<"Nao eh perfeitoz\n";
+    }
+    return 0;
+
+}
