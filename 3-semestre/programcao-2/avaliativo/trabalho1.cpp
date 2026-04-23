48e55a666d5756edfb462fc72f7468ade32bc016
PuFh <137668343+PuFh@users.noreply.github.com>
2026-04-23 10:53:45 -0300

    making atividade pro 2

---
+++ 3-semestre/programcao-2/avaliativo/trabalho1.cpp
@@ -0,0 +1,44 @@
+/*
+Em um vetor de 15 números, identifique e exiba a maior sequência de números pares
+consecutivos.
+*/
+#include <iostream>
+
+#define TAM 15
+
+int main(){
+    int vetor[TAM];
+    int maior,ax,sequencia;
+
+    for(int i = 0; i < 15; i++){
+        std::cout << "Digite o valor na posicao "<< i+1 <<": ";
+        std::cin >> vetor[i];
+    }
+
+    std::cout <<"-----------\n";
+    std::cout << "[";
+    for(int i = 0; i < TAM; i++){
+        std::cout <<vetor[i]<<" ";
+    }
+    std::cout << "]\n";
+
+    maior = 0;
+    ax = 0;
+    for(int i = 0; i < TAM;i++){
+        if(vetor[i] % 2 == 0){
+            ax++;
+        }else{
+            if(ax > maior){
+                maior = ax;
+            }
+            sequencia = maior;
+            ax = 0;
+        }
+    }
+    if(ax > maior){
+        maior = ax;
+    }
+    sequencia = maior;
+    
+    std::cout << "Maior Sequencia: "<<sequencia<<std::endl;
+}
