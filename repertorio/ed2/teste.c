73e24f2ab057a9ae0472ba5260547973695eaae1
Matheus <137668343+PuFh@users.noreply.github.com>
2026-06-09 10:38:23 -0300

    makuing exercise

---
+++ repertorio/ed2/teste.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+#define INT_MAX 32000
+typedef int bool;
+typedef int TIPOPESO;
+
+typedef struct adjacencia{
+    int vertice;
+    TIPOPESO peso;
+    struct adjacencia *prox;
+} ADJACENCIA;
+
+typedef struct vertice{
+    int grau;
+    /* Dados armazenados vao aqui */
+    ADJACENCIA *cab;
+} VERTICE;
+
+typedef struct grafo {
+    int vertices;
+    int arestas;
+    VERTICE *adj;
+} GRAFO;
+
+/* Criando um grafo */
+GRAFO *criarGrafo(int v){
+    GRAFO *g = (GRAFO *) malloc(sizeof(GRAFO));
+    
+    g->vertices     = v;
+    g->arestas      = 0;
+    g->adj          = (VERTICE *) malloc(v*sizeof(VERTICE));
+    int i;
+    
+    for (i=0; i<v; i++){
+        g->adj[i].cab = NULL;
+        g->adj[i].grau=0;
+    }
+    
+    return g;
+}
+
+ADJACENCIA *criaAdj(int v,int peso){
+    ADJACENCIA *temp = (ADJACENCIA *) malloc(sizeof(ADJACENCIA));
+    temp->vertice   = v;
+    temp->peso      = peso;
+    temp->prox      = NULL;
+    return (temp);
+}
+
+ADJACENCIA *inseriListAdj(ADJACENCIA *adj, int vi, int vf, TIPOPESO p)
+{
+
+    ADJACENCIA *ant = adj;
+    ADJACENCIA *atual = adj;
+    int control = 0;
+
+    ADJACENCIA *novo = criaAdj(vf, p);
+
+    while (atual != NULL && atual->vertice < vf)
+    {
+        ant = atual;
+        atual = atual->prox;
+        control = 1;
+    }
+    if (ant == NULL && control == 0)
+    {
+        novo->prox = atual;
+        
+        return novo;
+    }
+    else if (control == 0)
+    {
+        novo->prox = atual;
+        return novo;
+    }else{
+        novo->prox = atual;
+        ant->prox = novo;
+    }
+
+    return adj;
+}
+
+bool criaAresta(GRAFO *gr, int vi, int vf, TIPOPESO p){
+    if (!gr)
+        return(false);
+    if((vf<0) || (vf >= gr->vertices))
+        return(false);
+    if((vi<0) || (vf >= gr->vertices))
+        return(false);
+    
+    gr->adj[vi].cab = inseriListAdj(gr->adj[vi].cab, vi, vf, p);
+    gr->adj[vf].cab = inseriListAdj(gr->adj[vf].cab, vf, vi, p);
+    
+    gr->arestas++;
+    return (true);
+}
+
+
+
+void imprime(GRAFO *gr){
+    printf("Vertices: %d. Arestas: %d, \n", gr->vertices,gr->arestas);
+    
+    int i;
+    for(i=0;i<gr->vertices; i++){
+        printf("v%d: ",i);
+        ADJACENCIA *ad = gr->adj[i].cab;
+        while(ad){
+            printf("v%d(%d) ", ad->vertice,ad->peso);
+            ad = ad->prox;
+        }
+        
+        printf("\n");
+    }
+}
+
+// ----------- PRIM ---------------------------
+void agm_prim(GRAFO *gr, int orig, int *pai){
+   // implemente o algoritmo de prim
+   TIPOPESO *p = (TIPOPESO*)malloc(size)
+}
+
+int main(){
+    
+    // crie o main para fazer a leitura do arquivo
+    
+    return 0;
+}
