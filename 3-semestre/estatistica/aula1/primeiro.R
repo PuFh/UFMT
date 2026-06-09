95221186cec8c5f28803487cef4f4a8b09c9ddd3
Matheus <137668343+PuFh@users.noreply.github.com>
2026-06-08 21:20:07 -0300

    studing lang. R

--- 3-semestre/estatistica/aula1/primeiro.R
+++ 3-semestre/estatistica/aula1/primeiro.R
@@ -1,4 +1,14 @@
-x <- 18
+#copiar o arquivo milsa para este arqivo para trabalharmos em cima dele
+milsa <- read.table("milsa.txt",head = T)#Comando head define q a 1 liha é um cabechalo
+milsa
+head(milsa)
 
-print(x)
-
+milsa$salario 
+#faço com que o valor 1 = solteiro desta coluna, e o valor 2 = casado
+milsa $civil <- factor(milsa$civil, label = c("solteiro", "casado"), levels = 1:2)
+#mesma coisa
+milsa$instrucao <- factor(milsa$instrucao, label = c("1 grau","2 grau","superiro"),lev=1:3, ord = T)
+#mesma coisa
+milsa$regiao <- factor(milsa$regiao, labels = c("Captial","Interior","Outro"), lev = c(2,1,3))
+ 
+is.factor(milsa$regiao)
