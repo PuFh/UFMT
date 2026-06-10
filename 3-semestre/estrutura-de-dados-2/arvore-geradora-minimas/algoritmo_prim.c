3839c497d1d2b273ca57e9d8a1aedf20a61967c3
Matheus <137668343+PuFh@users.noreply.github.com>
2026-06-10 07:47:20 -0300

    AGM

--- 3-semestre/estrutura-de-dados-2/arvore-geradora-minimas/algoritmo_prim.c
+++ 3-semestre/estrutura-de-dados-2/arvore-geradora-minimas/algoritmo_prim.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define true 1
 #define false 0
-#define INT_MAX 32000
-typedef int bool;
+#define INT_INFITO INT_MAX/* a variavel do jeito q tava conflitando com um constante de uma das biblioteca, pois o a varivel com o nome INT-MAX ja existia
+entao pra resolver o problema e manter oq se espera, q é um valor infinito apra todos os vertice, eu troquei o nome para INT_INFINITO e fiz ela receber o maior valor possivel
+que seria o INT_MAX, Ai n conflita com nada
+*/
+
+//typedef int bool; tava dando erro na compilacao
 typedef int TIPOPESO;
 typedef struct adjacencia{
     int vertice;
@@ -111,9 +116,38 @@ void agm_prim(GRAFO *gr, int orig, int *pai){
    // implemente o algoritmo de prim
    //h = vai ser o nosso grafo dos vertices
    //u = o vertice atual q estamos
-   //peso
    //v = viznhos de u
    //s = novo grafo com os caminhos minimos da AGM
+   // ------ Irei me basear no pseudo-codigo dos slides
+   //peso -> tem que receber o vertices
+   // visitado -> se foi ou n visitado
+    TIPOPESO* peso = (TIPOPESO*)malloc(gr->vertices*sizeof(TIPOPESO));
+    bool* visitado = (bool*)malloc(gr->vertices * sizeof(bool));// 0 se n foi e 1 se foi
+    int* pai = (int*)malloc(gr->vertices*sizeof(int));
+    //-----INICIALIZACAO
+    //iremos percorrer todos os vertices e iremos fazer eles serem infinito, aqui vamos demorar muito tempo
+    for(int i = 0; i < gr->vertices;i++){
+        peso[i] = INT_INFITO;
+        visitado[i] = 0;
+        pai[i] = -1;
+    }
+    peso[orig] = 0;/*
+    seria o primeiro vertice que vamos fazer, e como ele é o primeiro ele tem peso 0
+    foi que nem o grafo que fizemos em sala de aula, esse seria o nosso 'A', e como ele ' é o prmeiro ele tem
+    pai NULL e peso 0, no caso n precisamo fazer ele -1 pq ja fizemos isso no laco em cima
+    */
+    /*
+    controi heap minimo A com V(G) (com base em p), isso ta no slide
+    */
+   int contador = gr->vertices;
+   while(contador >= 1){
+    //percorrer todos os vertices vizinhos
+    for()
+
+
+    contador--;
+   }
+
 }
 
 int main(){
