86c1bb26b610125ca39b76567799ec1ffcff2f4b
Matheus <137668343+PuFh@users.noreply.github.com>
2026-08-17 10:30:46 -0300

    Making exercise from aula 2

---
+++ 4-semestre/programacao-3/aula2-17-08/ex2/bool.java
@@ -0,0 +1,31 @@
+public class bool {
+    public static void main(String[] args){
+        //declaracao das variaveis booleanas
+        boolean n1,n2,n3;
+
+        //atribuicao de valores
+        n1= false;
+        n2 = true;
+        n3 = false;
+
+        //validacao das variaveis
+            //n1
+        if(n1)
+            System.out.println("variavel 1 eh verdadeira!");
+        else
+            System.out.println("Variavel 1 eh falsa!");
+
+            //n2
+        if(n2)
+            System.out.println("Varivel 2 eh verdadeira!");
+        else
+            System.out.println("Varivel 2 eh falsa");
+
+            //n3
+        if(n3)
+            System.out.println("Varivel 3 eh verdadeira!");
+        else
+            System.out.println("Variavel 3 eh falsa!");
+
+    }
+}
