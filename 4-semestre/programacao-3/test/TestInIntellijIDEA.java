31381ea16982377fba83f1173a538bb08148044d
Matheus <137668343+PuFh@users.noreply.github.com>
2026-08-22 00:36:10 -0300

    Create TestInIntellijIDEA.java

---
+++ 4-semestre/programacao-3/test/TestInIntellijIDEA.java
@@ -0,0 +1,8 @@
+public class TestInIntellijIDEA {
+    public static void main(String[] args){
+        System.out.println("Teste bilubliu");
+        System.out.println("Teste ui");
+        //final testa
+
+    }
+}
