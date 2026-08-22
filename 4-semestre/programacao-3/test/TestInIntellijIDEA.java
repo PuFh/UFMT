3388ee955afd810457e09b5772ec00a0e846ea13
Matheus <137668343+PuFh@users.noreply.github.com>
2026-08-21 23:02:41 -0300

    Final test commit

--- 4-semestre/programacao-3/test/TestInIntellijIDEA.java
+++ 4-semestre/programacao-3/test/TestInIntellijIDEA.java
@@ -2,6 +2,7 @@ public class TestInIntellijIDEA {
     public static void main(String[] args){
         System.out.println("Teste bilubliu");
         System.out.println("Teste ui");
+        //final test
 
     }
 }
